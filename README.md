ee6ce07bc3ea84e18a67566d3c9823aa70f7d7b6
DanYan97 <158104062+DanYan97@users.noreply.github.com>
2024-10-22 14:59:19 +0000

    First update

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ recursive call considers.
 Analyse the time complexity of your implementation and give a $\Theta$ bound for
 its worst-case runtime. Add your answer, including your reasoning, to this
 markdown file.
+
+The stack is used to simulate the recursive function calling. Initilize the stack takes $O(1)$; push the initial boundaries also taks $O(1)$; In the while loop, the loop continues as long as there are subarray to process, the total number of iterations in the worst case is $O(n)$, if the array is already sorted. Pop high and low element from the stack takes $O(1)$, and the if check takes $O(1)$ as well; Select the pivot as low takes $O(1)$; In the for loop, it iterate through the subarray from low+1 to high and compare each element with the pivot, in the worst case, the time complexity is $O(n)$, if the pivot only split off one element at each time. Outside the for the loop, move pivot to next position takes $O(1)$, and the push also takes $O(1)$.
+
+Therefore, combine together, the time complexity of the worst case is $\Theta(n^2)$
--- code.js
+++ code.js
@@ -1,3 +1,40 @@
 function quicksort(array) {
+
+    //use stack to manupulate the elements 
+    var stack = [];
+
+    //store the boundires in the stack in order to partition
+    stack.push(0);
+    stack.push(array.length - 1);
+
+    while (stack.length > 0) {
+        var high = stack.pop();
+        var low = stack.pop();
+
+        if (low >= high) continue; // base case, subarray with one or zero elements
+
+        // select first element as the pivot
+        var pivot = low;
+
+        // partition the array
+        for (var i = low + 1; i <= high; i++) {
+            if (array[i] < array[low]) {
+                // increment pivot and swap elements
+                pivot++;
+                [array[pivot], array[i]] = [array[i], array[pivot]];
+            }
+        }
+
+        // move the pivot 
+        [array[low], array[pivot]] = [array[pivot], array[low]];
+
+        // push the subarrays onto the stack 
+        stack.push(low);
+        stack.push(pivot - 1);
+
+        stack.push(pivot + 1);
+        stack.push(high);
+    }
+
     return array;
 }
